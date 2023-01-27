5afd7303a0790de2cb82c4307a5da3468598d24d
Michael Remijan <mjremijan@yahoo.com>
2023-01-27 15:25:27 -0600

    Added comments to JUnit tests, preparing for blog post.

--- src/test/java/org/thoth/jwt/JwtSymmetricalHMACwithSHA256Test.java
+++ src/test/java/org/thoth/jwt/JwtSymmetricalHMACwithSHA256Test.java
@@ -11,17 +11,24 @@ import org.junit.jupiter.api.Test;
 
 /**
  *
- * @author mjrem
+ * @author Michael Remijan <mjremijan@yahoo.com>
  */
 public class JwtSymmetricalHMACwithSHA256Test {
     
     @Test
     public void testHMACwithSHA256() throws Exception
     {
+        // This sets up an HMAC key with a secret value which 
+        // should remain known only to the Authentication Server
+        // and trusted clients.
         String secret = "thisismysupersecretkeywhichshouldonlybeontheauthenticationserver";
         String algorithm  = "HmacSHA256";
         SecretKeySpec key = new SecretKeySpec(secret.getBytes(), algorithm);
         
+        // Here the jjwt library is used to generate a JWT. 
+        // Remember a JWT is in the form of xxxx.yyyy.zzzz 
+        // I'm creating this object so I can use it as the
+        // an expected values for the JUnit tests.
         String [] tokens = 
             Jwts.builder().setSubject("adam")
            .setExpiration(new Date(2018, 1, 1))
@@ -31,25 +38,42 @@ public class JwtSymmetricalHMACwithSHA256Test {
            .compact().split("\\.")
         ;
         
-        // HEADER
+        // JWT HEADER
+        // Given the following JSON header, encode it and 
+        // assert it with the encoded header created by jjwt.
         String header = "{\"alg\":\"HS256\"}";
-        String expectedEncodedHeader = tokens[0];
         String actualEncodedHeader = Base64.getUrlEncoder().withoutPadding().encodeToString(header.getBytes());
-        assertEquals(expectedEncodedHeader, actualEncodedHeader);
+        assertEquals(tokens[0], actualEncodedHeader);
         
-        // PAYLOAD
+        // JWT PAYLOAD
+        // Given the following JSON payload, encode it and 
+        // assert it with the encoded payload created by jjwt.
         String payload = "{\"sub\":\"adam\",\"exp\":61475608800,\"iss\":\"info@wstutorial.com\",\"groups\":[\"user\",\"admin\"]}";
-        String expectedEncryptedPayload = tokens[1];
         String actualEncodedPayload = Base64.getUrlEncoder().withoutPadding().encodeToString(payload.getBytes());
-        assertEquals(expectedEncryptedPayload, actualEncodedPayload);
+        assertEquals(tokens[1], actualEncodedPayload);
         
         // SIGNATURE / VERIFY
-        String expectedEncryptedSignature = tokens[2];
+        // For HMAC, which is a symmetric algorithm, the
+        // signature and verify steps are the same because
+        // both rely on the same key (see above). The key
+        // is created using a secret value (see above) and
+        // this secret value must be kept secret and only
+        // available on the Authentication Server. The key
+        // is used to create the signature for the JWT. Clients
+        // Typically will make an authentication request (HTTPS)
+        // to the Authentication server to verify a JWT. Clients
+        // cannot verify a JWT themselves because they do not
+        // have access to the secret value used to create
+        // the HMAC key. However, if a Client is 100% trusted,
+        // The secret value can be shared with the Client so
+        // that the Client can do its own verification. 
+        // WARNING: This means the Client will also be able 
+        // to make new JWTs, which can be dangerous.
         Mac mac = Mac.getInstance("HmacSHA256");
         mac.init(key);
         String actualEncryptedSignature = Base64.getUrlEncoder().withoutPadding().encodeToString(
             mac.doFinal((actualEncodedHeader + "." + actualEncodedPayload).getBytes())
         );
-        assertEquals(expectedEncryptedSignature, actualEncryptedSignature);
+        assertEquals(tokens[2], actualEncryptedSignature);
     }
 }
